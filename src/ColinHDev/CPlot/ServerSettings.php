18a28376dc5ea814e28ddfec43df42f8ec2200c7
ColinHDev <colinheidfeld@gmail.com>
2022-05-22 18:31:47 +0200

    ServerSettings: Added getWorldPassways() method

--- src/ColinHDev/CPlot/ServerSettings.php
+++ src/ColinHDev/CPlot/ServerSettings.php
@@ -7,7 +7,9 @@ namespace ColinHDev\CPlot;
 use ColinHDev\CPlot\plots\BasePlot;
 use ColinHDev\CPlot\worlds\WorldSettings;
 use pocketmine\math\AxisAlignedBB;
+use pocketmine\math\Facing;
 use pocketmine\utils\SingletonTrait;
+use pocketmine\world\World;
 
 class ServerSettings {
     use SingletonTrait;
@@ -18,6 +20,8 @@ class ServerSettings {
     private int $z;
     /** @phpstan-var array<string, AxisAlignedBB> */
     private array $worldAABBs = [];
+    /** @phpstan-var array<string, array<int, AxisAlignedBB[]>> */
+    private array $worldPassways = [];
 
     public function __construct(string $name, int $x, int $z) {
         $this->name = $name;
@@ -62,5 +66,59 @@ class ServerSettings {
         );
         $this->worldAABBs[$worldName] = $aabb;
         return clone $aabb;
+    }
+
+    /**
+     * @phpstan-return array<int, AxisAlignedBB[]>
+     */
+    public function getWorldPassways(string $worldName, WorldSettings $worldSettings) : array {
+        if (isset($this->worldPassways[$worldName])) {
+            return $this->worldPassways[$worldName];
+        }
+        $worldBorder = $this->getWorldBorder($worldName, $worldSettings);
+        $roadPlotLength = $worldSettings->getRoadSize() + $worldSettings->getPlotSize();
+        $borderLength = ($roadPlotLength) * $this->worldSize + $worldSettings->getRoadSize();
+        $x = $this->x * $borderLength;
+        $z = $this->z * $borderLength;
+        $this->worldPassways[$worldName] = [];
+        $this->worldPassways[$worldName][Facing::NORTH] = [];
+        $this->worldPassways[$worldName][Facing::SOUTH] = [];
+        $this->worldPassways[$worldName][Facing::WEST] = [];
+        $this->worldPassways[$worldName][Facing::EAST] = [];
+        for ($i = 0; $i <= $this->worldSize; $i++) {
+            $this->worldPassways[$worldName][Facing::NORTH][] = new AxisAlignedBB(
+                ($x + $i * $roadPlotLength) + 1,
+                World::Y_MIN,
+                $worldBorder->minZ,
+                ($x + $i * $roadPlotLength) + $worldSettings->getRoadSize() - 1,
+                World::Y_MAX,
+                $worldBorder->minZ
+            );
+            $this->worldPassways[$worldName][Facing::SOUTH][] = new AxisAlignedBB(
+                ($x + $i * $roadPlotLength) + 1,
+                World::Y_MIN,
+                $worldBorder->maxZ,
+                ($x + $i * $roadPlotLength) + $worldSettings->getRoadSize() - 1,
+                World::Y_MAX,
+                $worldBorder->maxZ
+            );
+            $this->worldPassways[$worldName][Facing::WEST][] = new AxisAlignedBB(
+                $worldBorder->minX,
+                World::Y_MIN,
+                ($z + $i * $roadPlotLength) + 1,
+                $worldBorder->minX,
+                World::Y_MAX,
+                ($x + $i * $roadPlotLength) + $worldSettings->getRoadSize() - 1
+            );
+            $this->worldPassways[$worldName][Facing::EAST][] = new AxisAlignedBB(
+                $worldBorder->maxX,
+                World::Y_MIN,
+                ($z + $i * $roadPlotLength) + 1,
+                $worldBorder->maxX,
+                World::Y_MAX,
+                ($x + $i * $roadPlotLength) + $worldSettings->getRoadSize() - 1
+            );
+        }
+        return $this->worldPassways[$worldName];
     }
 }
