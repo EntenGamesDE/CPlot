c0bdc2bc4929cf7e5a7a58c5772875f641d18271
ColinHDev <colinheidfeld@gmail.com>
2022-06-01 17:37:39 +0200

    CPlotTeleportPacket: Updated properties and added support for automatic plot claiming

--- src/ColinHDev/CPlot/packet/CPlotTeleportPacket.php
+++ src/ColinHDev/CPlot/packet/CPlotTeleportPacket.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace ColinHDev\CPlot\packet;
 
+use ColinHDev\CPlot\commands\PlotCommand;
+use ColinHDev\CPlot\commands\subcommands\ClaimSubcommand;
 use ColinHDev\CPlot\plots\Plot;
+use ColinHDev\CPlot\plots\TeleportDestination;
 use ColinHDev\CPlot\provider\DataProvider;
 use matze\cloudbridge\network\packets\DataPacket;
 use matze\cloudbridge\network\packets\types\PlayerTransferPacket;
@@ -25,10 +28,8 @@ class CPlotTeleportPacket extends PlayerTransferPacket {
     public int $plotX;
     // The Z coordinate of the plot.
     public int $plotZ;
-    // Whether the player should be teleported to the center of the plot.
-    public bool $teleportToPlotCenter;
-    // Whether the player should be teleported to the set spawn of the plot.
-    public bool $checkPlotSpawnFlag;
+    // The destination where the player should be teleported to. A list of destinations can be found in {@see PlotTeleportDestination}.
+    public int $teleportDestination;
     // Whether or not the plot should be automatically claimed by the player after the transfer.
     public bool $claimPlotAfterTransfer;
 
@@ -38,20 +39,19 @@ class CPlotTeleportPacket extends PlayerTransferPacket {
      * @param string $plotWorldName         The name of the world the plot is on.
      * @param int $plotX                    The X coordinate of the plot.
      * @param int $plotZ                    The Z coordinate of the plot.
-     * @param bool $teleportToPlotCenter    Whether the player should be teleported to the center of the plot.
-     * @param bool $checkPlotSpawnFlag      Whether the player should be teleported to the set spawn of the plot.
+     * @param int $teleportDestination      The destination where the player should be teleported to. A list of destinations can be found in {@see PlotTeleportDestination}.
+     * @phpstan-param TeleportDestination::* $teleportDestination
      * @param bool $claimPlotAfterTransfer  Whether or not the plot should be automatically claimed by the player after the transfer.
      * @return self                         Returns a new instance of this packet with the given values as properties.
      */
-    public static function create(string $playerName, string $plotServerName, string $plotWorldName, int $plotX, int $plotZ, bool $teleportToPlotCenter, bool $checkPlotSpawnFlag, bool $claimPlotAfterTransfer) : self {
+    public static function create(string $playerName, string $plotServerName, string $plotWorldName, int $plotX, int $plotZ, int $teleportDestination, bool $claimPlotAfterTransfer) : self {
         $packet = new self;
         $packet->playerName = $playerName;
         $packet->plotServerName = $plotServerName;
         $packet->plotWorldName = $plotWorldName;
         $packet->plotX = $plotX;
         $packet->plotZ = $plotZ;
-        $packet->teleportToPlotCenter = $teleportToPlotCenter;
-        $packet->checkPlotSpawnFlag = $checkPlotSpawnFlag;
+        $packet->teleportDestination = $teleportDestination;
         $packet->claimPlotAfterTransfer = $claimPlotAfterTransfer;
         return $packet;
     }
@@ -69,7 +69,14 @@ class CPlotTeleportPacket extends PlayerTransferPacket {
                 if (!$player instanceof Player) {
                     return;
                 }
-                $plot->teleportTo($player, $packet->teleportToPlotCenter, $packet->checkPlotSpawnFlag);
+                $plot->teleportTo($player, $packet->teleportDestination);
+                if ($packet->claimPlotAfterTransfer) {
+                    $plotCommand = $server->getCommandMap()->getCommand("plot");
+                    assert($plotCommand instanceof PlotCommand);
+                    $claimSubcommand = $plotCommand->getSubcommandByName("claim");
+                    assert($claimSubcommand instanceof ClaimSubcommand);
+                    yield from $claimSubcommand->execute($player, []);
+                }
             }
         );
     }
